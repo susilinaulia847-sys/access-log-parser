ee98690e57015b2d4da79182477f5959edb49af0
Julia Sushilina <susilinaulia847@gmail.com>
2025-09-07 23:40:20 +0400

    Изменение кода в файле Main.java для ДЗ по синтаксису языка. Сложение, вычитание, умножение и частное двух чисел

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,18 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Случайное число от 0 до 1: " + Math.random());
+        System.out.print("Введите первое число: ");
+        int firstNumber = new Scanner(System.in).nextInt();
+        System.out.print("Введите второе число: ");
+        int secondNumber = new Scanner(System.in).nextInt();
+        int sum = firstNumber + secondNumber;
+        int difference = firstNumber - secondNumber;
+        int multiplication = firstNumber * secondNumber;
+        double quotient = (double) firstNumber / secondNumber;
+        System.out.println("Сумма: " + sum);
+        System.out.println("Разность: " + difference);
+        System.out.println("Произведение: " + multiplication);
+        System.out.println("Частное: " + quotient);
     }
 }
