0bc1771c6d2719ace5a1b6c6384e82a8477ea2d1
Julia Sushilina <susilinaulia847@gmail.com>
2025-09-30 23:54:06 +0400

    Задание по теме "Циклы"

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,28 @@
+import java.io.File;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Случайное число от 0 до 1: " + Math.random());
+        int sum = 0;
+        while (true) {
+            System.out.print("Введите путь к файлу: ");
+            String path = new Scanner(System.in).nextLine();
+            File file = new File(path);
+            boolean fileExists = file.exists();
+            boolean isDirectory = file.isDirectory();
+            if (fileExists && isDirectory) {
+                System.out.println("Файл найден, но это папка");
+                continue;
+            }
+            if (!fileExists && !isDirectory) {
+                System.out.println("Путь указан неверно, файл не найден");
+                continue;
+            }
+            if (fileExists) {
+                System.out.println("Путь указан верно");
+                sum++;
+                System.out.println("Это файл номер" + " " + sum);
+            }
+        }
     }
 }
